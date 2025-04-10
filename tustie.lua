134aabcbb627aa9ccceef0fca7531bb5dc96915a
Ahzuontop <149396163+Ahzuontop@users.noreply.github.com>
2025-04-11 06:17:44 +0700

    Delete tustie.lua

--- tustie.lua
+++
@@ -1,5 +0,0 @@
-if game:GetService("Players").LocalPlayer.UserId == 123456789 then -- thay UserId đúng nếu cần
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Ahzuontop/TusTie/main/reach.lua"))()
-else
-    game.Players.LocalPlayer:Kick("Unauthorized access.")
-end
